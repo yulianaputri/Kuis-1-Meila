05fdf296a966b78b8122f41ad3f12b0e520d54a2
stefenSitorus <antonius.stefen@gmail.com>
2018-10-09 16:31:47 +0700

    first commit

---
+++ app/src/main/java/com/example/indieglo/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.indieglo.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, berita1.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/indieglo/myapplication/berita2.java
@@ -0,0 +1,13 @@
+package com.example.indieglo.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class berita2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berita2);
+    }
+}
